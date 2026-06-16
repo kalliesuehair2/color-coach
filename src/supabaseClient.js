16ced790f59fd96f8a8a3e9d6c191c60f66c0ebc
kalliesuehair2 <ngr99dyj5v@privaterelay.appleid.com>
2026-06-15 21:57:42 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jcgaeiqzqsivdtwpgqcf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZ2FlaXF6cXNpdmR0d3BncWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDQ3MjgsImV4cCI6MjA5NjUyMDcyOH0.3CtjfOc9Q7i3Y3OtWqAXQzDnXR6d0F366kDm8DHk0Sg';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
